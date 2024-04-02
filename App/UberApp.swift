5af3a9d9f32caf4318d322f7e6cac221ff199224
Ashwin Kumar K <44889005+ashwin-kumar-k@users.noreply.github.com>
2024-04-02 19:21:20 +0530

    Add files via upload

---
+++ App/UberApp.swift
@@ -0,0 +1,18 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Ashwin Kumar on 14/02/24.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            MainTabBar()
+        }
+    }
+}
